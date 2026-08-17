ae2dac724657bf0c33bf1163905c88864968e8c0
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-08-17 23:56:44 +0530

    Simple Commit

--- 07_Star_pyramid_pattern7.cpp
+++ 07_Star_pyramid_pattern7.cpp
@@ -2,5 +2,24 @@
 #include<string>
 
 int main(){
-    
+    int n;
+    std::cin >> n;
+    int spacing_variable;
+    // std::cout << n;
+    for (int i = 0; i <= (n*2); i++)
+    {
+        spacing_variable = n;
+        for (spacing_variable; spacing_variable <= i; spacing_variable)
+        {
+            std::cout << "0";
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            
+            std::cout << "*";
+        }
+        
+        std::cout << std::endl;
+    }
+    return 0;
 }
