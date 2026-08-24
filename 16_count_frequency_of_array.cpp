cabab0fdf7042882c831f5e44a9990baad69355c
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-08-25 00:37:29 +0530

    Started 16

---
+++ 16_count_frequency_of_array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <unordered_map>
+#include <map>
+#include <string>
+
+int main()
+{
+    int array_to_calculate[5] = {10, 15, 10, 13902, 0};
+    int size_of_array = array_to_calculate.size();
+    std::cout << size_of_array;
+    return 0;
+}
