f8376ef5b55535e0524bce6963a847d472b4ad98
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-08-25 23:45:49 +0530

    19 Boiler Plate

---
+++ 19_bubble_sort.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+std::array<int, 5> sort_array(std::array<int, 5> array_to_sort)
+{
+    int size_of_array = array_to_sort.size();
+
+    return array_to_sort;
+}
+
+int main()
+{
+    std::array<int, 5> question_array = {13, 12, 45, 75, 32};
+    std::array<int, 5> solution_array = sort_array(question_array);
+
+    // debug print
+    int n = solution_array.size();
+    cout << "After selection sort: " << "\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << solution_array[i] << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
