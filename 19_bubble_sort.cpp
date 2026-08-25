43b9dc5817c20e57489b1c8bbd5d97e16e7a0bd4
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-08-26 01:27:32 +0530

    Finished 19

--- 19_bubble_sort.cpp
+++ 19_bubble_sort.cpp
@@ -4,6 +4,16 @@ using namespace std;
 std::array<int, 5> sort_array(std::array<int, 5> array_to_sort)
 {
     int size_of_array = array_to_sort.size();
+    for (int i = 0; i < size_of_array - 1; i++)
+    {
+        for (int j = 0; j < size_of_array - 1; j++)
+        {
+            if (array_to_sort[j] > array_to_sort[j + 1])
+            {
+                std::swap(array_to_sort[j], array_to_sort[j + 1]);
+                        }
+        }
+    }
 
     return array_to_sort;
 }
