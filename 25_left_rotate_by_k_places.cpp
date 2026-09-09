504534277ef11c3578f091c642ead70ade9bfaa5
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-09-09 22:48:47 +0530

    Finished 25

---
+++ 25_left_rotate_by_k_places.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+int main(){
+    std::array<int, 5> array_to_shift = {1, 2, 3, 4, 5};
+    int number_of_places_to_shift = 2;
+    // std::array<int, 5> shifted_array ;
+    for (int k = 0; k < number_of_places_to_shift; k++)
+    {
+            int temporary_first_variable_of_array ;
+    for (int i = 0; i < array_to_shift.size(); i++)
+    {
+        if (i == 0)
+        {
+            temporary_first_variable_of_array = array_to_shift[i];
+        }
+        else if (i == (array_to_shift.size() - 1) )
+        {
+            array_to_shift[(i - 1)] = array_to_shift[i];
+            array_to_shift[i] = temporary_first_variable_of_array;
+        }
+        
+        else{
+            array_to_shift[(i - 1)] = array_to_shift[i];
+        }
+        
+    }
+    }
+    
+    for (int j = 0; j < array_to_shift.size(); j++)
+    {
+        std::cout << "Index " << j << " is: " << array_to_shift[j] << std::endl;
+    }
+    
+    return 0;
+}
